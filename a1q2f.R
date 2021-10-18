97709b469710913efad71ab1518859188b9537ee
jasoncnm <64656764+jasoncnm@users.noreply.github.com>
2021-10-18 12:04:33 -0400

    f
    A standard deck of cards consists of 52 cards, of which there are 13 cards in each of 4 “suits”, Clubs |,
    Diamonds }, Hearts ~, and Spades , and the 13 cards in each suit have “ranks” in order of 2, 3, 4, 5, 6, 7,
    8, 9, 10, Jack, Queen, King, and Ace. In the game Texas Hold’em, each player is initially dealt a two-card
    “hand”, also known as “hole cards”, at random from the deck without replacement.
    
    Using R, simulate 10,000 draws of Texas Hold’em hands
    Compute the percentage of those draws in
    which the cards have the same suit.

---
+++ a1q2f.R
@@ -0,0 +1,20 @@
+set.seed(20910986)
+
+deck = expand.grid(c("A","2","3","4","5","6","7","8","9","10","J","Q","K"),
+                   c("Clubs","Diamonds","Hearts","Spades"))
+colnames(deck) = c("Card Rank", "Card Suit")
+
+nSim = 100000
+success = 0
+for(val in 1:nSim)
+{
+  hands = deck[sample(1:nrow(deck),2,replace = F),]
+  draw1 = hands[1,2]
+  draw2 = hands[2,2]
+  if (draw1 == draw2)
+  {
+    success = success+1
+  }
+}
+probest = success/nSim
+print(probest)
